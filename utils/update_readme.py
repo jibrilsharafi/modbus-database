8ba42b66edb2d7892bd237c2ef78e45b45f4b7bc
Jibril Sharafi <jibril.sharafi@gmail.com>
2024-06-04 23:55:22 +0200

    Massive device addition + minor changes and better formatting/linting

--- utils/update_readme.py
+++ utils/update_readme.py
@@ -8,6 +8,8 @@ logging.basicConfig(
     format="[%(asctime)s] [%(levelname)s] [%(funcName)s] %(message)s",
 )
 
+MODBUS_TABLE_DIRECTORY = 'modbus-tables'
+README_PATH = "README.md"
 
 def get_device_metadata(directory: str) -> List[Dict[str, Any]]:
     """
@@ -24,49 +26,106 @@ def get_device_metadata(directory: str) -> List[Dict[str, Any]]:
                 metadata["path"] = os.path.relpath(root, directory).replace("\\", "/")
                 if "sample" not in metadata["manufacturer"].lower():
                     devices.append(metadata)
-                    logging.info(
+                    logging.debug(
                         f"Loaded metadata from {os.path.join(root, 'metadata.json')}"
                     )
     return devices
 
 
-def format_devices(devices: List[Dict[str, Any]]) -> str:
+def format_devices(devices: List[Dict[str, Any]]) -> list[str]:
     """
     Formats the device information into a Markdown table.
 
     :param devices: A list of dictionaries representing the metadata of each device.
     :return: A string representing the formatted device information.
     """
-    formatted = "| Manufacturer | Model | Industry | Application |\n"
-    formatted += "|--------------|-------|----------|-------------|\n"
-    for device in sorted(devices, key=lambda x: (x["manufacturer"], x["model"])):
-        formatted += f"| **[{device['manufacturer']}](modbus-tables/{device['manufacturer']})** | [{device['model']}](modbus-tables/{device['manufacturer']}/{device['model']}) | {device['tags']['industry']} | {device['tags']['application']} |\n"
+    formatted = []
+    formatted.append("| Manufacturer | Model | Industry | Application |")
+    formatted.append("|--------------|-------|----------|-------------|")
+    for device in sorted(devices, key=lambda x: (x["manufacturer"].lower(), x["model"].lower())):
+        formatted.append(
+            f"| **[{device['manufacturer']}]({device['path']})** | [{device['model']}]({device['path']}/{device['model']}) | {device['tags']['industry']} | {device['tags']['application']} |"
+        )
     return formatted
 
+def find_start_end_table(readme: List[str]) -> tuple[int, int]:
+    """
+    Finds the start and end of the table in the README.
 
-if __name__ == "__main__":
-    devices = get_device_metadata("modbus-tables")
-    formatted_devices = format_devices(devices)
-
-    with open("README.md", "r") as f:
-        readme = f.readlines()
-
-    # Find the start and end of the previous table
-    start = next(
+    :param readme: A list of strings representing the README.
+    :return: A tuple of integers representing the start and end of the table.
+    """
+    start_table = next(
         i
         for i, line in enumerate(readme)
         if "| Manufacturer | Model | Industry | Application |" in line
     )
-    end = (
-        next(i for i, line in enumerate(readme[start:], start) if line.strip() == "")
-        + 1
-    )
+    end_table = next(
+        i
+        for i, line in enumerate(readme)
+        if line.strip().startswith("## Usage")
+    ) - 1
 
+    return start_table, end_table
+
+def update_table() -> None:
+    """
+    Updates the device information in the README.
+    """
+    devices = get_device_metadata(MODBUS_TABLE_DIRECTORY)
+    formatted_devices = format_devices(devices)
+
+    with open(README_PATH, "r") as f:
+        readme = f.readlines()
+
+    start_table, end_table = find_start_end_table(readme)
+    
     # Replace the previous table with the new one
-    readme[start:end] = formatted_devices.split("\n")
+    readme[start_table:end_table] = formatted_devices
     # Between start and end, add newline every other line
-    readme[start:end] = [line + "\n" for line in readme[start:end]]
+    readme[start_table:end_table] = [line + "\n" for line in readme[start_table:end_table]]
 
-    with open("README.md", "w") as f:
+    with open(README_PATH, "w") as f:
         f.writelines(readme)
         logging.info("Updated README.md with device information")
+
+def count_devices() -> int:
+    """
+    Counts the number of devices.
+
+    :param devices: A list of dictionaries representing the metadata of each device.
+    :return: An integer representing the number of devices.
+    """
+    return len(get_device_metadata(MODBUS_TABLE_DIRECTORY))
+
+def update_count() -> None:
+    """
+    Updates the devices count in the README.
+
+    :param devices: An integer representing the number of devices.
+    """
+    BADGE_MARKDOWN = "[![Devices Count](https://img.shields.io/badge/Devices"
+    BADGE_COLOR = "blue"
+    BADGE_LINK = "#devices"
+    
+    total_devices = count_devices()
+    
+    with open(README_PATH, "r") as f:
+        readme = f.readlines()
+    
+    for i, line in enumerate(readme):
+        if BADGE_MARKDOWN in line:
+            readme[i] = f"{BADGE_MARKDOWN}-{total_devices}-{BADGE_COLOR})]({BADGE_LINK})\n"
+            break
+
+    with open(README_PATH, "w") as f:
+        f.writelines(readme)
+        logging.info("Updated device count in README.md")
+
+if __name__ == "__main__":
+    logging.info("Updating README.md")
+    
+    update_table()
+    update_count()
+    
+    logging.info("Finished updating README.md")
